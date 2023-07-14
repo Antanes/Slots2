28504a6d06928c534f12b3e3f1c8acb8891d8def
Antanes <venam.i-com@mail.ru>
2023-07-14 09:21:39 +0500

    Добавьте файлы проекта.

--- Slots/Models/Domain/Machine.cs
+++ Slots/Models/Domain/Machine.cs
@@ -6,23 +6,21 @@
         public static int Change { get; set; }
         public void Purchase(Drink drink)
         {
-            if (drink.Price < Sum && drink.Quantity > 0)
+            if (drink.Price <= Sum && drink.Quantity > 0)
             {
                 drink.Quantity--;
                 Sum -= drink.Price;
-                Change = Sum;
-                ChangePrice = Sum;
+                Change = Sum;          
+                Coin10 = Sum / 10;
+                Sum -= Coin10 * 10;
+                Coin5 = Sum / 5;
+                Sum -= Coin5 * 5;
+                Coin2 = Sum / 2;
+                Sum -= Coin2 * 2;
+                Coin1 = Sum;
                 Sum = 0;
-                Coin10 = ChangePrice / 10;
-                ChangePrice -= Coin10 * 10;
-                Coin5 = ChangePrice / 5;
-                ChangePrice -= Coin5 * 5;
-                Coin2 = ChangePrice / 2;
-                ChangePrice -= Coin2 * 2;
-                Coin1 = ChangePrice;
             }
-        }
-        public static int ChangePrice { get; set; } = 0;
+        }        
         public static int Coin1 { get; set; } = 0;
         public static int Coin2 { get; set; } = 0;
         public static int Coin5 { get; set; } = 0;
